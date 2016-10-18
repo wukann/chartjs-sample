4d1a9ba93612d2216d8003d8a338a1b2941e88bc
wukann <wukann@gmail.com>
2016-10-18 21:25:00 +0900

    change title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,16 +18,16 @@ describe('App: ChartjsSample', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Chart sample.'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Chart sample.');
   }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Chart sample.');
   }));
 });
